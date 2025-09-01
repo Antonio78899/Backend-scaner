dce43cd5f1674b4b235551fa1d2b9d4a18a6288d
Antonio78899 <antonio_cortez999@hotmail.com>
2025-09-01 11:18:22 -0500

    corregido

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const { crearTablaCodigos } = require("./controllers/codigosController");
-
-const PORT = process.env.SERVER_PORT || 3002;
-
-app.listen(PORT, async () => {
-  await crearTablaCodigos();
-  console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
-});
